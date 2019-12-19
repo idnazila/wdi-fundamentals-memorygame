da7fe6dd7c2f846a63178d2ca1672bb169e9703e
Ali Zandi <idnazila.com@gmail.com>
2019-12-19 13:11:34 -1000

    Step 6: Functions Project

--- memorygame/js/main.js
+++ memorygame/js/main.js
@@ -1,17 +1,18 @@
 let cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
 
-let cardOne = cards[0];
-let cardTwo = cards[2];
-
-cardsInPlay.push(cardOne, cardTwo);
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert('Sorry, try again.');
-	}
+const checkForMatch = () => {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        console.log("You found a match!");
+    } else {
+        console.log('Sorry, try again.');
+    }
 }
 
-console.log("User flipped " + cardOne);
+const flipCard = cardId => {
+    checkForMatch();
+    console.log("User flipped " + cards[cardId]);
+    cardsInPlay.push(cards[cardId]);
+}
+
+flipCard(2);
