78cf529746bd037dc40c54fa9290bc41ec47ebf9
Ali Zandi <idnazila.com@gmail.com>
2019-12-19 13:03:09 -1000

    Step 5: Control Flow Project

--- memorygame/js/main.js
+++ memorygame/js/main.js
@@ -1,6 +1,17 @@
-const cardOne = 'queen';
-const cardTwo = 'king';
-const cardThree = 'queen';
-const cardFour = 'king';
+let cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
 
-console.log('User flipped ' + cardOne);
+let cardOne = cards[0];
+let cardTwo = cards[2];
+
+cardsInPlay.push(cardOne, cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert('Sorry, try again.');
+	}
+}
+
+console.log("User flipped " + cardOne);
