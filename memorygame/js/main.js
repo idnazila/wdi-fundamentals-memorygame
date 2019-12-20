57c8cbbb318696a9977946d9c20be4e9b9545dcd
Ali Zandi <idnazila.com@gmail.com>
2019-12-19 14:19:02 -1000

    Step 7: Objects

--- memorygame/js/main.js
+++ memorygame/js/main.js
@@ -1,4 +1,25 @@
-let cards = ["queen", "queen", "king", "king"];
+let cards = [{
+        rank: 'queen',
+        suit: 'hearts',
+        cardImage: 'images/queen-of-hearts.png'
+    },
+    {
+        rank: 'queen',
+        suit: 'diamonds',
+        cardImage: 'images/queen-of-diamonds.png'
+    },
+    {
+        rank: 'king',
+        suit: 'hearts',
+        cardImage: 'images/king-of-hearts.png'
+    },
+    {
+        rank: 'king',
+        suit: 'diamonds',
+        cardImage: 'images/king-of-diamonds.png'
+    }
+];
+
 let cardsInPlay = [];
 
 const checkForMatch = () => {
@@ -11,8 +32,10 @@ const checkForMatch = () => {
 
 const flipCard = cardId => {
     checkForMatch();
-    console.log("User flipped " + cards[cardId]);
-    cardsInPlay.push(cards[cardId]);
+    console.log("User flipped " + cards[cardId].rank);
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
+    cardsInPlay.push(cards[cardId].rank);
 }
 
-flipCard(2);
+flipCard(1);
